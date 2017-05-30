61cc9dd310b17fac5770895e43b2509c30a0d1c3
Chamara Singhabahu <mails.chamara@gmail.com>
2017-05-30 10:42:11 +0530

    Change library support to google drive

--- src/main/java/org/wso2/carbon/esb/connector/GoogleDriveConnector.java
+++ src/main/java/org/wso2/carbon/esb/connector/GoogleDriveConnector.java
@@ -24,13 +24,13 @@ import org.wso2.carbon.connector.core.ConnectException;
 /**
  * Sample method implementation.
  */
-public class GooglePlusConnector extends AbstractConnector {
+public class GoogleDriveConnector extends AbstractConnector {
 
     @Override
     public void connect(MessageContext messageContext) throws ConnectException {
         Object templateParam = getParameter(messageContext, "generated_param");
         try {
-            log.info("GooglePlus sample connector received message :" + templateParam);
+            log.info("GoogleDrive sample connector received message :" + templateParam);
             /**Add your connector code here 
             **/
         } catch (Exception e) {
